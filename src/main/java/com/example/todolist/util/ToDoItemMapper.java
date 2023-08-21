8d20cf396dc74877c4dbdb84ddb7413c6713cec2
armenAsatryan <armen.asatryan21@gmail.com>
2023-08-21 18:43:25 +0400

    cleanup

--- src/main/java/com/example/todolist/util/ToDoItemMapper.java
+++ src/main/java/com/example/todolist/util/ToDoItemMapper.java
@@ -8,46 +8,33 @@ import com.example.todolist.model.ToDoItemModel;
 
 public class ToDoItemMapper {
 
-    public ToDoItemDTO modelToDto (ToDoItemModel toDoItemModel){
-        ToDoItemDTO toDoItemDTO =new ToDoItemDTO();
+    public ToDoItemDTO modelToDto(ToDoItemModel toDoItemModel) {
+        ToDoItemDTO toDoItemDTO = new ToDoItemDTO();
         toDoItemDTO.setTitle(toDoItemModel.getTitle());
         toDoItemDTO.setCompleted(toDoItemModel.isCompleted());
         return toDoItemDTO;
     }
 
-    public ToDoItemModel dtoToModel (ToDoItemDTO toDoItemDTO){
-        ToDoItemModel toDoItemModel =new ToDoItemModel();
-        toDoItemModel.setTitle(toDoItemDTO.getTitle());
-        toDoItemModel.setCompleted(toDoItemDTO.isCompleted());
-        return toDoItemModel;
-    }
-
-    public ToDoItemEntity dtoToEntity (ToDoItemDTO toDoItemDTO){
-        ToDoItemEntity toDoItemEntity =new ToDoItemEntity();
+    public ToDoItemEntity dtoToEntity(ToDoItemDTO toDoItemDTO) {
+        ToDoItemEntity toDoItemEntity = new ToDoItemEntity();
         toDoItemEntity.setTitle(toDoItemDTO.getTitle());
         toDoItemEntity.setCompleted(toDoItemDTO.isCompleted());
         return toDoItemEntity;
     }
 
-    public ToDoItemDTO entityToDto (ToDoItemEntity toDoItemEntity){
-        ToDoItemDTO toDoItemDTO =new ToDoItemDTO();
+    public ToDoItemDTO entityToDto(ToDoItemEntity toDoItemEntity) {
+        ToDoItemDTO toDoItemDTO = new ToDoItemDTO();
         toDoItemDTO.setCompleted(toDoItemEntity.isCompleted());
         toDoItemDTO.setTitle(toDoItemEntity.getTitle());
         return toDoItemDTO;
     }
 
-    public ToDoItemEditDTO editModelToEditDto (ToDoItemEditModel toDoItemModel){
-        ToDoItemEditDTO toDoItemDTO =new ToDoItemEditDTO();
+    public ToDoItemEditDTO editModelToEditDto(ToDoItemEditModel toDoItemModel) {
+        ToDoItemEditDTO toDoItemDTO = new ToDoItemEditDTO();
         toDoItemDTO.setId(toDoItemModel.getId());
         toDoItemDTO.setTitle(toDoItemModel.getTitle());
         toDoItemDTO.setCompleted(toDoItemModel.isCompleted());
         return toDoItemDTO;
     }
 
-    public ToDoItemEntity editDtoToEntity (ToDoItemEditDTO toDoItemDTO){
-        ToDoItemEntity toDoItemEntity =new ToDoItemEntity();
-        toDoItemEntity.setTitle(toDoItemDTO.getTitle());
-        toDoItemEntity.setCompleted(toDoItemDTO.isCompleted());
-        return toDoItemEntity;
-    }
 }
